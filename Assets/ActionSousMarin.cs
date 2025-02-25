4a3bff288fdc6168bbeed08700d82af861b6a3e7
Cassostiguy <cassouostiguy@gmail.com>
2025-02-25 16:35:31 -0500

    petit changement script

--- Assets/ActionSousMarin.cs
+++ Assets/ActionSousMarin.cs
@@ -5,12 +5,24 @@ using UnityEngine.InputSystem;
 
 public class ActionSousMarin : MonoBehaviour
 {
+
+    [SerializeField] private float vitesseMouvement;
+
+    private Rigidbody _rb;
+
+    private Vector3 directionInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
+    void OnMove(InputValue directionBase)
+    {
+        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * vitesseMouvement;
+        directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
+    }
     // Update is called once per frame
     void Update()
     {
