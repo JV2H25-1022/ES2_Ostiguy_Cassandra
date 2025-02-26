0cd2d24392e86406944662a03e67bac33bde653e
Cassostiguy <cassouostiguy@gmail.com>
2025-02-25 19:37:56 -0500

    ajout script mais pas ce qu'etait attendu

--- Assets/ActionSousMarin.cs
+++ Assets/ActionSousMarin.cs
@@ -24,8 +24,27 @@ public class ActionSousMarin : MonoBehaviour
         directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
     }
     // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        Vector3 mouvement = directionInput;
+
+         if (directionInput.magnitude > 0f)
+        {
+            // calculer rotation cible
+            float rotationCible = Vector3.SignedAngle(Vector3.forward, directionInput.normalized , Vector3.up);
+            // appliquer la rotation cible directement
+            _rb.MoveRotation(Quaternion.Euler(0.0f, rotationCible, 0.0f));
+            _rb.AddRelativeForce(0, 0, mouvement.magnitude, ForceMode.VelocityChange);
+        }
+    }
+
+   
+
+
     void Update()
     {
         
     }
-}
+
+ }
