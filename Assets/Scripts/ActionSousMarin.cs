c46ed20622e8234b09f9ef8bf423795db5826ef9
Cassostiguy <cassouostiguy@gmail.com>
2025-02-28 16:58:45 -0500

    animation fonctionne, manque vitesse a ajuster et a linverse

--- Assets/Scripts/ActionSousMarin.cs
+++ Assets/Scripts/ActionSousMarin.cs
@@ -6,34 +6,34 @@ using UnityEngine.InputSystem;
 public class ActionSousMarin : MonoBehaviour
 {
 
-    [SerializeField] private float vitesseMouvement;
+    [SerializeField] private float _vitesseMouvement;
 
     
 
     private Rigidbody _rb;
 
-    private Vector3 directionInput;
+    private Vector3 _directionInput;
 
     public InputActionReference move;
 
-    private Animator animator;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        vitesseMouvement = 1;
-        animator = GetComponent<Animator>();
+        _vitesseMouvement = 1;
+        _animator = GetComponent<Animator>();
     }
 
 
     void OnAccelerer(InputValue etatBouton)
      {
          if(etatBouton.isPressed) {
-            vitesseMouvement = 2;
+            _vitesseMouvement = 2;
        }
          else{
-             vitesseMouvement = 1;
+             _vitesseMouvement = 1;
          }
      }
     // Update is called once per frame
@@ -41,13 +41,41 @@ public class ActionSousMarin : MonoBehaviour
     
     private void Update()
      {
-        directionInput = move.action.ReadValue<Vector3>();
-       
-     }
+        _directionInput = move.action.ReadValue<Vector3>();
+
+
+
+        //Vertical
+        if (_directionInput.y > 0){
+            _animator.SetBool("MvtVertical", true);
+        }
+        else if (_directionInput.y < 0)
+        {
+            _animator.SetBool("MvtVertical", true);
+        }
+        else
+        {
+            _animator.SetBool("MvtVertical", false);
+        }
+
+        //Horizontal
+        if (_directionInput.z > 0)
+        {
+            _animator.SetBool("MvtHorizontal", true);
+        }
+        else if (_directionInput.z < 0)
+        {
+            _animator.SetBool("MvtHorizontal", true);
+        }
+        else
+        {
+            _animator.SetBool("MvtHorizontal", false);
+        }
+    }
     private void FixedUpdate()
     {
-        //Vector3 mouvement = directionInput;
-        _rb.velocity = new Vector3(directionInput.x * vitesseMouvement, directionInput.y * vitesseMouvement, directionInput.z * vitesseMouvement);
+        
+        _rb.velocity = new Vector3(_directionInput.x * _vitesseMouvement, _directionInput.y * _vitesseMouvement, _directionInput.z * _vitesseMouvement);
 
     }
 
