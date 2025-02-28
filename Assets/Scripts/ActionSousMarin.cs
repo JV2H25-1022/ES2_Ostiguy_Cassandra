d673d5439a0ac01aab6f2369953386d81f5ab2e3
Cassostiguy <cassouostiguy@gmail.com>
2025-02-28 14:14:01 -0500

    input fonctionne pour les fleche

--- Assets/Scripts/ActionSousMarin.cs
+++ Assets/Scripts/ActionSousMarin.cs
@@ -6,12 +6,15 @@ using UnityEngine.InputSystem;
 public class ActionSousMarin : MonoBehaviour
 {
 
-    [SerializeField] private float vitesseMouvement = 1;
+    [SerializeField] private float vitesseMouvement;
 
+    
 
     private Rigidbody _rb;
 
     private Vector3 directionInput;
+
+    public InputActionReference move;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +22,11 @@ public class ActionSousMarin : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(InputValue directionBase)
-    {
-        Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * vitesseMouvement;
-        directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
-    }
+   // void OnMove(InputValue directionBase)
+   // {
+      //  Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * vitesseMouvement;
+     //   directionInput = new Vector3(directionAvecVitesse.x, 0f, directionAvecVitesse.y);
+   // }
 
     // void OnAccelerer(InputValue etatBouton)
     // {
@@ -36,20 +39,21 @@ public class ActionSousMarin : MonoBehaviour
     // }
     // Update is called once per frame
 
-    void FixedUpdate()
-    {
-        Vector3 mouvement = directionInput;
-
-       
     
+    private void Update()
+     {
+        directionInput = move.action.ReadValue<Vector3>();
+        print(directionInput);
+     }
+    private void FixedUpdate()
+    {
+        //Vector3 mouvement = directionInput;
+        _rb.velocity = new Vector3(directionInput.x * vitesseMouvement, directionInput.y * vitesseMouvement, directionInput.z * vitesseMouvement);
+
     }
 
    
 
 
-    // void Update()
-    // {
-        
-    // }
 
  }
